7807562199b9fa8614e5dfab82e2cb008a96ecde
Ibrahim <ibrahimbahraj@gmail.com>
2022-10-18 10:43:47 -0400

    area_of_rectangle done

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,7 @@
+// Calculating the area of a rectangle
+
+var lenght = 25;
+var width = 25;
+var area_of_rectangle = lenght * width;
+
+console.log("The area of a rectangle is " + area_of_rectangle);
