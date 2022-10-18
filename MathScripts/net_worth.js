8d62d353192d6e88172bd8a7f4964aae6ac32f57
Ibrahim <ibrahimbahraj@gmail.com>
2022-10-18 10:31:49 -0400

    tips & networth completed

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,6 @@
+
+var assets = 375000;
+var debts = 110000;
+var net_worth = assets - debts;
+
+console.log ("Your networth is " + net_worth); 
---
+++ tips.js
@@ -0,0 +1,25 @@
+var foodCost = 79.25;
+var tax = 6.54;
+var tips = 12.00;
+
+var totalDue= foodCost + tax + tips;
+
+console.log("The total due is " + totalDue);
+
+console.log("Food cost is" + foodCost + " and Tax is " + tax);
+
+// console.log("Tips " + tips);
+console.log("Tips is " + tips.toFixed(2));
+
+console.log("Total Due is " + totalDue);
+
+
+
+
+
+
+
+
+
+
+
