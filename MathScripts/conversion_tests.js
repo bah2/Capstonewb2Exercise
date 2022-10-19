0c02ecb0ac59c3e99faaa7d4072fd5ac180c56d4
Ibrahim <ibrahimbahraj@gmail.com>
2022-10-19 10:13:48 -0400

    conversion

---
+++ MathScripts/conversion_tests.js
@@ -0,0 +1,62 @@
+
+// Converting 
+var a = " 101.1 "; 
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+var a1 = parseInt(a);
+console.log("ParseInt(" + a + "): " + a1)
+var b1 = parseInt(b);
+console.log("ParseInt(" + b + "): " + b1)
+var c1 = parseInt(c);
+console.log(c)
+var d1 = parseInt(d);
+console.log(d)
+
+// var a = " 101.1 "; 
+// var b = "55";
+// var c = "402 Stevens";
+// var d = "Number 5 ";
+
+
+
+var a2 = parseFloat(a)
+console.log(a)
+var b2 = parseFloat(b)
+console.log(b)
+var c2 = parseFloat(c)
+console.log(c)
+var d2 = parseFloat(d)
+console.log(d)
+
+
+// var a = " 101.1 "; 
+// var b = "55";
+// var c = "402 Stevens";
+// var d = "Number 5 ";
+
+var a3 = Number(a)
+console.log(a)
+var b3 = Number(b)
+console.log(b)
+var c3 = Number(c)
+console.log(c)
+var d3 = Number(d)
+console.log(d)
+
+var a = " 101.1 "; 
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+var a4 = +a
+console.log(a)
+var b4 = +b
+console.log(b)
+var c4 = +c
+console.log(c)
+var d4 = +d
+console.log(d)
+
+
