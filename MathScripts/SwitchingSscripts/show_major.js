14ab58c9c89012565a485f1f770e0196505bdaf3
Ibrahim <ibrahimbahraj@gmail.com>
2022-10-20 13:32:29 -0400

    series of exercises

---
+++ MathScripts/SwitchingSscripts/show_major.js
@@ -0,0 +1,36 @@
+// Switch statements
+// major code as cases
+let studentName = "Betty";
+let deptName;
+let NameOfMajor;
+
+
+
+switch (deptName) {
+  case "BIOL":
+    deptOffice = "Science Bldg, Room 310";
+    NameOfMajor = "Biology";
+    break;
+  case "CSCI":
+    deptOffice = "Shepard Hall, Room 314";
+    NameOfMajor = "Computer Science";
+    break;
+  case "ENG":
+    deptOffice = "Kerr Hall, Room 210";
+    NameOfMajor = "English";
+    break;
+  case "HIST":
+    deptOffice = "Kerr Hall, Room 114";
+    NameOfMajor = "History";
+    break;
+  case "MKT":
+    deptOffice = "West Hall, Room 310";
+    NameOfMajor = "Marketing";
+    break;
+    default:
+    dayName = "Not part of us";
+    break;
+}
+// console.log("Student: " + studentName + "\n"+ " Major:" + NameOfMajor + "\n" + "Advising Location:" + deptName);
+
+// console.log("Student Name:" + studentName + "\n" +"Student major:" + NameOfMajor )
